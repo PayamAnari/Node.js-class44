5c2786861363afb60c46669f407d759ad8865734
PayamAnari <anari.p62@gmail.com>
2023-08-06 21:32:01 +0200

    Add homework week1 node.js

---
+++ homework/hackyourtemperature/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('hello from backend to frontend!');
+});
+
+app.post('/weather', (req, res) => {
+  const cityName = req.body.cityName;
+  if (!cityName) {
+    res.status(404).send('City name is required!');
+  } else {
+    res.send(`City name ${cityName} added!`);
+  }
+});
+
+app.listen(PORT, () =>
+  console.log(`Server running on port: http://localhost:${PORT}`),
+);
