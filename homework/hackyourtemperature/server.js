c37da8cc7c0b3fb955766c6e0285c23725e69b48
PayamAnari <anari.p62@gmail.com>
2023-08-08 14:15:06 +0200

    edited

--- homework/hackyourtemperature/server.js
+++ homework/hackyourtemperature/server.js
@@ -1,8 +1,7 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('hello from backend to frontend!');
